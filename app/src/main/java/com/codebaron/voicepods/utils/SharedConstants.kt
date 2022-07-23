efeceb9b0f89a6bb2e94edd6bf629c0856ab319a
Anyanwu Nicholas <83093834+codeBaron-dev@users.noreply.github.com>
2022-07-23 22:34:01 +0100

    Added SharedConstants.kt for adding dynamic constants or variables

---
+++ app/src/main/java/com/codebaron/voicepods/utils/SharedConstants.kt
@@ -0,0 +1,19 @@
+package com.codebaron.voicepods.utils
+
+/**
+ * @author Anyanwu Nicholas(codeBaron)
+ * @since Jul 23 - 2022
+ * @NoteToFutureMe this file is used for adding shared constants or variables that will be used
+ * across the app
+ */
+
+val GENRES = listOf(
+    "Games",
+    "Business",
+    "Eduction",
+    "AR & VR",
+    "Health Care",
+    "Religion",
+    "Military",
+    "Technology"
+)
