9fa2a21bab4d079dac05b36cb4444967285ebbb6
Anyanwu Nicholas <83093834+codeBaron-dev@users.noreply.github.com>
2022-07-23 22:25:04 +0100

    Added api endPoints in ApiEndPoint.kt

---
+++ app/src/main/java/com/codebaron/voicepods/repository/ApiEndPoint.kt
@@ -0,0 +1,22 @@
+package com.codebaron.voicepods.repository
+
+import com.codebaron.voicepods.models.genre.PodcastGenres
+import com.codebaron.voicepods.models.podx.Podcasts
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.HeaderMap
+import retrofit2.http.Query
+
+interface ApiEndPoint {
+
+    @GET(FULL_TEXT_SEARCH)
+    suspend fun searchPodcast(
+        @HeaderMap headers: Map<String, String>,
+        @Query("q", encoded = true) searchContent: String,
+        @Query("language", encoded = true) language: String,
+        @Query("safe_mode", encoded = true) safetyMode: String,
+    ): Response<Podcasts>
+
+    @GET(GENRES)
+    suspend fun getPodcastGenre(@HeaderMap headers: Map<String, String>): Response<PodcastGenres>
+}
