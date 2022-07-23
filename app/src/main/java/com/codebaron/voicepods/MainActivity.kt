969213d6bca66ed3cb6579cca0d951df5b5e2d01
Anyanwu Nicholas <83093834+codeBaron-dev@users.noreply.github.com>
2022-07-23 21:32:41 +0100

    Started new project, added reuired dependencies, created packages, created a kotlin file and added some apis

---
+++ app/src/main/java/com/codebaron/voicepods/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.codebaron.voicepods
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.codebaron.voicepods.ui.theme.VoicePodsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VoicePodsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    VoicePodsTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/codebaron/voicepods/repository/ServicesEndPoint.kt
@@ -0,0 +1,19 @@
+package com.codebaron.voicepods.repository
+
+/**
+ * @author Anyanwu Nicholas
+ * @since Jul 23 - 2022
+ * @NoteForFutureMe This kotlin file holds all apis used in this project,
+ * please be deliberate when making modifications to an api else something breaks
+ */
+const val BASE_URL = "https://listen-api.listennotes.com/api/v2"
+const val FULL_TEXT_SEARCH = "search"
+const val TYPE_HEAD_SEARCH = "typeahead"
+const val TRENDING_SEARCHES = "trending_searches"
+const val RELATED_SEARCHES = "related_searches"
+const val SPELL_CHECK_SEARCH = "spellcheck"
+const val BEST_PODCASTS = "best_podcasts"
+const val GENRES = "genres"
+const val COUNTRIES = "regions"
+const val LANGUAGES = "languages"
+const val RANDOM_PODCASTS_EPISODES = "just_listen"
