2cbd3f95969ecbc64d4bc7659f4e38579e445591
Anyanwu Nicholas <83093834+codeBaron-dev@users.noreply.github.com>
2022-07-23 22:56:28 +0100

    Added VoicePodApp.kt and new field to AndroidManifest.xml

---
+++ app/src/main/java/com/codebaron/voicepods/module/VoicePodApp.kt
@@ -0,0 +1,11 @@
+package com.codebaron.voicepods.module
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author Anyanwu Nicholas
+ * @since Jul 23 - 2022
+ */
+@HiltAndroidApp
+class VoicePodApp: Application()
